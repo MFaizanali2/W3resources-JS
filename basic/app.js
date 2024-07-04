9b23bcf4c27ef412f4a98d7720ef07ddd336f6f3
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-04 20:50:40 +0500

    comment question one

--- basic/app.js
+++ basic/app.js
@@ -1,8 +1,9 @@
     // Question One
-let day = new Date().getDay();
-let hour = new Date().getHours();
-let minutes = new Date().getMinutes();
-let second = new Date().getSeconds();
-console.log(`${day} ${hour}PM : ${minutes} : ${second}`)
+
+// let day = new Date().getDay();
+// let hour = new Date().getHours();
+// let minutes = new Date().getMinutes();
+// let second = new Date().getSeconds();
+// console.log(`${day} ${hour}PM : ${minutes} : ${second}`)
 
     // Question two
