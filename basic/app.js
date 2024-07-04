38c4d5ed1012b71871ea2afb5d303405dde346a0
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-04 21:47:02 +0500

    question 2 complete
    continue

--- basic/app.js
+++ basic/app.js
@@ -7,3 +7,24 @@
 // console.log(`${day} ${hour}PM : ${minutes} : ${second}`)
 
     // Question two
+
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth();
+let yyyy = today.getFullYear();
+
+if(dd < 10){
+    dd = "0" + dd;
+}
+if(mm < 10){
+    mm = "0" + mm;
+}
+
+today = mm + "-" + dd + "-" + yyyy
+console.log(today)
+today = mm + "/" + dd + "/" + yyyy
+console.log(today)
+today = dd + "-" + mm + "-" + yyyy
+console.log(today)
+today = yyyy + "/" + mm + "/" + dd
+console.log(today)
