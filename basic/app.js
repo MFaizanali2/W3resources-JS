ad8dd590420fc6692c08fecc651399d9130f9166
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-04 21:47:27 +0500

    comment question 2
    continue

--- basic/app.js
+++ basic/app.js
@@ -8,23 +8,23 @@
 
     // Question two
 
-let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth();
-let yyyy = today.getFullYear();
+// let today = new Date();
+// let dd = today.getDate();
+// let mm = today.getMonth();
+// let yyyy = today.getFullYear();
 
-if(dd < 10){
-    dd = "0" + dd;
-}
-if(mm < 10){
-    mm = "0" + mm;
-}
+// if(dd < 10){
+//     dd = "0" + dd;
+// }
+// if(mm < 10){
+//     mm = "0" + mm;
+// }
 
-today = mm + "-" + dd + "-" + yyyy
-console.log(today)
-today = mm + "/" + dd + "/" + yyyy
-console.log(today)
-today = dd + "-" + mm + "-" + yyyy
-console.log(today)
-today = yyyy + "/" + mm + "/" + dd
-console.log(today)
+// today = mm + "-" + dd + "-" + yyyy
+// console.log(today)
+// today = mm + "/" + dd + "/" + yyyy
+// console.log(today)
+// today = dd + "-" + mm + "-" + yyyy
+// console.log(today)
+// today = yyyy + "/" + mm + "/" + dd
+// console.log(today)
