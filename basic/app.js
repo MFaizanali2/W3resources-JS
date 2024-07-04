3e3b58066204124404e9f3e0a532e56376c164ff
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-04 20:50:02 +0500

    js line 8
    continue

--- basic/app.js
+++ basic/app.js
@@ -1,6 +1,8 @@
-    
+    // Question One
 let day = new Date().getDay();
 let hour = new Date().getHours();
 let minutes = new Date().getMinutes();
 let second = new Date().getSeconds();
 console.log(`${day} ${hour}PM : ${minutes} : ${second}`)
+
+    // Question two
