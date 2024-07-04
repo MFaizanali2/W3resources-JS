c1bd2fc6e52e0c590284189dae5149356c86be1f
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-04 20:48:39 +0500

    question one
    continue

---
+++ basic/app.js
@@ -0,0 +1,6 @@
+    
+let day = new Date().getDay();
+let hour = new Date().getHours();
+let minutes = new Date().getMinutes();
+let second = new Date().getSeconds();
+console.log(`${day} ${hour}PM : ${minutes} : ${second}`)
