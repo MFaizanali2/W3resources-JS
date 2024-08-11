fbcf9530031ab2975434f724eb3faf3e27090770
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-11 13:53:03 +0500

    Update app.js

--- lopps/app.js
+++ lopps/app.js
@@ -47,3 +47,53 @@ function sampleNumbers(){
 
 }
 sampleNumbers();
+
+
+// Qno:- 04) Write a JavaScript conditional statement to find the largest of five numbers. 
+// Display an alert box to show the results.
+// Sample numbers : -5, -2, -6, 0, -1
+
+
+
+// Qno:- 05) Write a JavaScript for loop that iterates from 0 to 15. For each iteration,
+//  it checks if the current number is odd or even, and displays a message on the screen.
+// Sample Output :
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+
+
+// Qno:- 06) Write a JavaScript program that computes the average marks of the 
+// following students. Then, this average is used to determine the corresponding grade.
+
+
+// Qno:- 07) Write a JavaScript program that iterates integers from 1 to 100.
+//  But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz".
+//  For numbers multiples of both three and five print "FizzBuzz".
+
+
+// Qno:- 08)  According to Wikipedia a happy number is defined by the following process :
+// "Starting with any positive integer, replace the number by the sum of the squares of its digits,
+//  and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a
+//   cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers,
+//    while those that do not end in 1 are unhappy numbers (or sad numbers)".
+// Write a JavaScript program to find and print the first 5 happy numbers.
+
+
+// Qno:- 09)  Write a JavaScript program to find the Armstrong numbers of 3 digits.
+// Note : An Armstrong number of three digits is an integer such that the sum of the cubes of its 
+// digits is equal to the number itself.
+//  For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
+
+
+// Qno:- 10)  Write a JavaScript program to construct the following pattern, using a nested for loop.
+
+
+// Qno:- 11) Write a JavaScript program to compute the greatest common divisor (GCD) of two positive integers.
+
+
+// Qno:- 12) Write a JavaScript program to sum 3 and 5 multiples under 1000.
+
+
+// Qno:- 13) Write a JavaScript program to find the sum of all prime numbers between 0 and 1000.
+
