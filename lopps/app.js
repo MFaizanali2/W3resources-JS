429e850fbd4763815821ee474f2498d1538e72ca
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-11 19:52:23 +0500

    Update app.js

--- lopps/app.js
+++ lopps/app.js
@@ -42,7 +42,15 @@ function sampleNumbers(){
     let num2 = prompt("Enter number two");
     let num3 = prompt("Enter number three");
     
-    
+    if(num1 > num2 && num1 > num3){
+        console.log(`${num1} is greater`)
+    }
+    else if(num2 > num1 && num2 > num3){
+        console.log(`${num2} is greater`)
+    }
+    else{
+        console.log(`${num3} is greater`)
+    }
 
 
 }
