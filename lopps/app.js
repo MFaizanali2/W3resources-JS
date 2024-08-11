8b2af6888029f4fcbc81225422c43a4a095db227
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-11 12:52:03 +0500

    Update app.js

--- lopps/app.js
+++ lopps/app.js
@@ -1,1 +1,14 @@
 // Qno:- 01)  Write a JavaScript program that displays the largest integer among two integers.
+function largestInteger(num1, num2){
+    if(num1 > num2){
+        console.log("one is greater")
+    }else
+     if(num2 > num1){
+        console.log("two is greater")
+    }
+    else{
+        console.log("both are equal")
+    }
+}
+
+let result = largestInteger(15, 15);
