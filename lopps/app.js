4532d64edc0573188891033d354a6ff347f1df6d
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-08-14 20:58:06 +0500

    Update app.js

--- lopps/app.js
+++ lopps/app.js
@@ -113,23 +113,23 @@
 // Qno:- 07) Write a JavaScript program that iterates integers from 1 to 100.
 //  But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz".
 //  For numbers multiples of both three and five print "FizzBuzz".
-function program(){
-    for(let i = 0; i<=100; i++){
-        if(i%3 == 0 && i%5 == 0){
-            console.log("FizzBuzz")
-        }
-        else if(i%3 == 0){
-            console.log("Fizz")
-        }
-        else if(i%5 == 0){
-            console.log("Buzz")
-        }
-        else{
-            console.log(i)
-        }
-    }
-}
-program()
+// function program(){
+//     for(let i = 0; i<=100; i++){
+//         if(i%3 == 0 && i%5 == 0){
+//             console.log("FizzBuzz")
+//         }
+//         else if(i%3 == 0){
+//             console.log("Fizz")
+//         }
+//         else if(i%5 == 0){
+//             console.log("Buzz")
+//         }
+//         else{
+//             console.log(i)
+//         }
+//     }
+// }
+// program();
 
 
 // Qno:- 08)  According to Wikipedia a happy number is defined by the following process :
